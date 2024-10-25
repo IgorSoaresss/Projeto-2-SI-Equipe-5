fd6819c9b469c75511e6dad710eeb311b25e2f0d
Eduardo Américo Bezerra da Silva <166758614+EduardoAmericoo@users.noreply.github.com>
2024-10-25 12:46:50 -0300

    Inicio da construção da home

--- projeto_TypeFlow/appTypeFlow/views.py
+++ projeto_TypeFlow/appTypeFlow/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     return HttpResponse("Olá, este é o TypeFlow!")
+
+@login_required
+def home_aluno(request):
+    # buscar dados relacionados ao aluno, como progresso, testes realizados...
+    context = {
+        'nome_aluno': request.user.first_name,  
+    }
+    return render(request, 'aluno/home.html', context)
