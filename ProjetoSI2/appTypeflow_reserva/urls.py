e6bc1fd5cf580de2eec67090586b675c2f5b833f
Caio Felipe da Silva Rodrigues <cfsr@cesar.school>
2024-10-25 17:04:38 -0300

    Adicionado Botao para Teste Mbti

--- ProjetoSI2/appTypeflow_reserva/urls.py
+++ ProjetoSI2/appTypeflow_reserva/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Exemplo de rota associando a view 'home'
+    path('teste-mbti/', views.teste_mbti, name='teste_mbti'),
 ]
--- ProjetoSI2/appTypeflow_reserva/views.py
+++ ProjetoSI2/appTypeflow_reserva/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'appTypeflow_reserva/home.html')
+
+def teste_mbti(request):
+    return render(request, 'appTypeflow_reserva/teste_mbti.html')
