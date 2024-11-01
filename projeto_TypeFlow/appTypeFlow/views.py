c7b6d49b88b4422ad86177a58979c3445cb7abd5
Eduardo Américo Bezerra da Silva <166758614+EduardoAmericoo@users.noreply.github.com>
2024-11-01 19:47:53 -0300

    Atualizações de caminho

--- projeto_TypeFlow/appTypeFlow/views.py
+++ projeto_TypeFlow/appTypeFlow/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-def base(request):
-    return render(request, 'base.html')
 
 def teste_personalidade(request):
     # Dados e lógica para o teste de personalidade
     return render(request, 'teste/teste_personalidade.html')
 
 def home_aluno(request):
-    return render(request, 'aluno/home.html')
+    return render(request, 'aluno/home_aluno.html')
--- projeto_TypeFlow/projeto_TypeFlow/urls.py
+++ projeto_TypeFlow/projeto_TypeFlow/urls.py
@@ -4,8 +4,7 @@ from appTypeFlow import views  # Certifique-se de que o app é appTypeFlow (ou a
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appTypeFlow/', include('appTypeFlow.urls')),  # Inclui URLs do app appTypeFlow
-    path('home/', views.home_aluno, name='home_aluno'),  # URL para home_aluno
+    path('appTypeFlow/', include('appTypeFlow.urls')),  # Inclui URLs do app appTypeFlow,  
     path('teste_personalidade/', views.teste_personalidade, name='teste_personalidade'),  # URL para teste de personalidade
     path('', views.home_aluno, name='home_aluno'),  # Define a home_aluno como a página inicia
 ]
