2919fb8f27fc27dc91e7b5d78d967f0ebce9db4e
IgorSoaresss <ijas@cesar.school>
2024-11-02 18:04:04 -0300

    update exclusão

--- TesteMBTI2-Feature/ProjetoSI2/appTypeflow_reserva/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class AppTypeflowReservaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appTypeflow_reserva'
-
--- TesteMBTI2-Feature/ProjetoSI2/appTypeflow_reserva/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('teste_mbti/', views.teste_mbti, name='teste_mbti'),
-    path('calcular_mbti/', views.calcular_mbti, name='calcular_mbti'),  # New route for MBTI calculation
-]
