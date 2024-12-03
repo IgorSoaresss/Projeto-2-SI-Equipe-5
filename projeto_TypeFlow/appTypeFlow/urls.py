62ef0df7dcc2c4060932496bbb0663eec5459573
Eduardo Américo Bezerra da Silva <166758614+EduardoAmericoo@users.noreply.github.com>
2024-12-02 22:34:26 -0300

    merge do login e cadastro

--- projeto_TypeFlow/appTypeFlow/urls.py
+++ projeto_TypeFlow/appTypeFlow/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('teste4_mbti/', views.teste4_mbti, name='teste4_mbti'),
     path('quiz/<int:page>/', views.quiz_view, name='quiz_view'),
     path('result/', views.result_view, name='result_view'),
+    path('cadastro.html/', views.cadastro, name='cadastro.html'),
+    path("login/", views.login_usuario, name="login_usuario"),
     ]
